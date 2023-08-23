aa82ba415bd70c3e4a22d6963c6ae2c1de4d737b
Roman <40907255+witelokk@users.noreply.github.com>
2023-08-23 16:25:14 +0300

    renderable.py refactor

--- md2gost/renderable/renderable.py
+++ md2gost/renderable/renderable.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections.abc import Generator
 from abc import ABC, abstractmethod
 
@@ -8,5 +9,5 @@ from ..rendered_info import RenderedInfo
 class Renderable(ABC):
     @abstractmethod
     def render(self, previous_rendered: RenderedInfo, layout_state: LayoutState)\
-            -> Generator["RenderedInfo | Renderable", None, None]:
+            -> Generator[RenderedInfo | Renderable, None, None]:
         """Renders the object to one or multiple Parented objects or Renderables to be rendered on the next page"""
