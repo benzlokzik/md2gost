82968f2aa2ded34d81684559f19d43669f5e6dbf
Roman <40907255+witelokk@users.noreply.github.com>
2023-08-27 14:08:44 +0300

    Update paragraph.py
    rewrite and fix parahraph render

--- md2gost/renderable/paragraph.py
+++ md2gost/renderable/paragraph.py
@@ -80,33 +80,19 @@ class Paragraph(Renderable):
             if layout_state.current_page_height == 0 and layout_state.page > 1:
                 height_data.before = 0
 
-            # height = 0
-            # base_line_height = height_data.before + ((lines - 1) * height_data.line_spacing + 1) * height_data.line_height
-            # for lines in range(height_data.lines):
-            #     if base_height <\
-            #             layout_state.remaining_page_height:
-            #         height += base_height
-            #         layout_state.add_height(base_height)
-            #         height += min(height_data.after, layout_state.remaining_page_height)
-            #     else:
-            #         if height_data.
-
-            if layout_state.current_page_height + height_data.before + height_data.base <= layout_state.max_height <\
-                    layout_state.current_page_height + height_data.before + height_data.base + height_data.after:
-                # height without space_after fits but with space_after doesn't, so height is remaining page space
-                height = layout_state.remaining_page_height
-            elif layout_state.current_page_height + height_data.base <= layout_state.max_height:
+            if height_data.base <= layout_state.remaining_page_height:
                 height = min(height_data.full, layout_state.remaining_page_height)
             else:
-                # still doesn't fit, so take remaining space and full height
                 fitting_lines = 0
                 for lines in range(1, height_data.lines):
                     if height_data.before + ((lines - 1) * height_data.line_spacing + 1) * height_data.line_height \
                             > layout_state.remaining_page_height:
                         break
                     fitting_lines += 1
+                
+                print(self._docx_paragraph.text[:50], fitting_lines, height_data.lines - fitting_lines)
 
-                if fitting_lines <= 1:
+                if fitting_lines <= 1 or (height_data.lines-fitting_lines == 1 and height_data.lines == 3):
                     # if only no or only one line fits the page, paragraph goes to the next page
                     height = layout_state.remaining_page_height + height_data.full
                 elif height_data.lines-fitting_lines == 1:
